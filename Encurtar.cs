4f94769780c27ff43a83beaafef058e9f1cd0f2a
Lucas Reis <lucas.dias@edu.uniso.br>
2017-11-09 23:10:04 -0200

    Init

---
+++ Encurtar.cs
@@ -0,0 +1,15 @@
+﻿namespace EncurtadorLink
+{
+    public class Encurtar
+    {
+
+        public string Url { get; set; }
+
+        public Encurtar(string url)
+        {
+            Url = url;
+        }
+
+        public string Encrypt() => $"{Url.GetHashCode():X}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncurtadorLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var padrao = "http://uni.so/";
+
+            // logo da uniso
+            using (StreamReader uniso = new StreamReader("uniso.txt"))
+            {
+                var line = uniso.ReadToEnd();
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("------------- ENCURTADOR DE URL UNISO -------------");
+            Console.WriteLine("Tenha o total conhecimento que as URLs depois de encurtadas, não funcionarão.");
+            Console.WriteLine("Este projeto tem finalidade didática.");
+
+            Console.WriteLine("\n\nInsira a URL que você deseja encurtar:");
+            var url = Console.ReadLine();
+           
+            Console.Clear();
+
+            // encurta a URL
+            var encurtar = new Encurtar(url);
+
+            // resultado
+            var result = encurtar.Encrypt();
+
+            Console.WriteLine("URL encriptada com sucesso! Para acessar, basta copiar e colar o link gerado abaixo em seu navegador!\n\n");
+            Console.WriteLine($"{padrao}{result}");
+            Console.WriteLine("\n\nObrigado por utilizar a nossa ferramenta!");
+            Console.ReadLine();
+
+        }
+    }
+}
