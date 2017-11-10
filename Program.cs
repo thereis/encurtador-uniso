d544f5a6efe9047762577f76b6d7a7f83f51aa4e
Lucas Reis <lucas.dias@edu.uniso.br>
2017-11-09 23:24:11 -0200

    Fixing empty string

--- Program.cs
+++ Program.cs
@@ -12,7 +12,9 @@ namespace EncurtadorLink
         static void Main(string[] args)
         {
 
+            // variaveis
             var padrao = "http://uni.so/";
+            var url = string.Empty;
 
             // logo da uniso
             using (StreamReader uniso = new StreamReader("uniso.txt"))
@@ -25,9 +27,14 @@ namespace EncurtadorLink
             Console.WriteLine("Tenha o total conhecimento que as URLs depois de encurtadas, não funcionarão.");
             Console.WriteLine("Este projeto tem finalidade didática.");
 
-            Console.WriteLine("\n\nInsira a URL que você deseja encurtar:");
-            var url = Console.ReadLine();
+            // verifica se a string está vazia ou nula
+            while (string.IsNullOrEmpty(url))
+            {
+                Console.WriteLine("\n\nInsira a URL que você deseja encurtar:");
+                url = Console.ReadLine();
+            }
            
+            // limpa o console
             Console.Clear();
 
             // encurta a URL
